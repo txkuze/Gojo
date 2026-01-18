5b688abacea3568379515c8b5dd51ecde3117899
UCHIHABOTS <txkuze@gmail.com>
2026-01-19 01:21:13 +0530

    Update start.py

--- Spy/plugins/bot/start.py
+++ Spy/plugins/bot/start.py
@@ -43,7 +43,7 @@ async def start_pm(client, message: Message, _):
             keyboard = first_page(_)
             return await message.reply_photo(
                 photo=config.START_IMG_URL,
-                has_spoiler=True
+                has_spoiler=True,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
@@ -160,4 +160,5 @@ async def welcome(client, message: Message):
                 await message.stop_propagation()
         except Exception as ex:
             print(ex)
+
 
